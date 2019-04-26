7bf870b707bad08fdcef9b61841d82b81e99ee1f
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-26 15:58:58 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillafinalfrontend/KodillaFinalFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillafinalfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaFinalFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaFinalFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillafinalfrontend/KodillaFinalFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.kodillafinalfrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaFinalFrontendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
