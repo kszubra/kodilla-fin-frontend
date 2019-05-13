c10d8fb01d6100aec230b099a4e57e4e8b94167e
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-05-13 14:55:59 +0200

    holding backend host address

---
+++ src/main/java/com/kodilla/kodillafinalfrontend/config/AdminConfig.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillafinalfrontend.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class AdminConfig {
+
+    @Value("${backend.host.address}")
+    private String backendHostAddress;
+}
