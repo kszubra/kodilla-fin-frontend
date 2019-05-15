5c814f642724a84436bef289212d6c246d5175e5
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-05-16 00:32:59 +0200

    works

--- src/main/java/com/kodilla/kodillafinalfrontend/MainView.java
+++ src/main/java/com/kodilla/kodillafinalfrontend/MainView.java
@@ -1,38 +1,34 @@
 package com.kodilla.kodillafinalfrontend;
 
 import com.kodilla.kodillafinalfrontend.backend.api.payment.PaymentFacade;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
+
 @Component
-@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE) //so it doesn't throw assertion error when refreshing site
+@UIScope
 @Route()
 public class MainView extends VerticalLayout { // address depends on class name, with  "PaymentView" address was http://localhost:8081/payment
-    @Autowired
-    private PaymentFacade paymentFacade;
+    private final PaymentFacade paymentFacade;
     private Grid<Payment> grid = new Grid<>(Payment.class);
 
     public void refresh() {
         grid.setItems( paymentFacade.getPayments() );
     }
 
-    public MainView() {
-        add( new Button("Show payments", e-> grid.setItems( paymentFacade.getPayments() ) ) );
+    public MainView(PaymentFacade paymentFacade) {
+        this.paymentFacade = paymentFacade;
+
+        //add( new Button("Show payments", e-> grid.setItems( paymentFacade.getPayments() ) ) );
         grid.setColumns("id", "value", "status", "paymentDate");
         add(grid);
 
         setSizeFull();
 
-        //refresh();
+        refresh();
     }
 
 
